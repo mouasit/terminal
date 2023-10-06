7812ab903d32607f09ff1c0bfc557f46a553d141
mouasit <mustaphaouassit60@gmail.com>
2023-10-06 01:54:21 +0100

    feature: add click tabs

--- src/Components/Tabs.jsx
+++ src/Components/Tabs.jsx
@@ -1,43 +1,54 @@
 import React from "react";
 import { CloseIcon, PlusIcon } from "./Icons";
 
+const TabsContext = React.createContext();
+
 export default function Tabs() {
+  const [clickTabIndex, setClickTabIndex] = React.useState(0);
   return (
-    <div className=" w-full flex flex-col gap-4">
-      <div className="flex gap-2 items-center">
-        <TabList>
-          <Tab active={true}>
-            Tab <span className="font-bold">01</span>
-          </Tab>
-          <Tab>
-            Tab <span className="font-bold">02</span>
-          </Tab>
-          <Tab>
-            Tab <span className="font-bold">03</span>
-          </Tab>
-        </TabList>
-        <button>
-          <PlusIcon className="w-6 h-6" />
-        </button>
+    <TabsContext.Provider
+      value={{
+        numberOfchildren: 0,
+        clickTabIndex: clickTabIndex,
+        setClickTabIndex: setClickTabIndex,
+      }}
+    >
+      <div className=" w-full flex flex-col gap-4">
+        <div className="flex gap-2 items-center">
+          <TabList>
+            <Tab>
+              Tab <span className="font-bold">01</span>
+            </Tab>
+            <Tab>
+              Tab <span className="font-bold">02</span>
+            </Tab>
+            <Tab>
+              Tab <span className="font-bold">03</span>
+            </Tab>
+          </TabList>
+          <button>
+            <PlusIcon className="w-6 h-6" />
+          </button>
+        </div>
+        <TabPanels>
+          <TabPanel>
+            <div className="bg-black rounded-md h-[30rem] text-white p-2">
+              /c/Users/mustapha --- 1 ~
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <div className="bg-black rounded-md h-[30rem] text-white p-2">
+              /c/Users/mustapha --- 2 ~
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <div className="bg-black rounded-md h-[30rem] text-white p-2">
+              /c/Users/mustapha --- 3 ~
+            </div>
+          </TabPanel>
+        </TabPanels>
       </div>
-      <TabPanels>
-        <TabPanel>
-          <div className="bg-black rounded-md h-[30rem] text-white p-2">
-            /c/Users/mustapha --- 1 ~
-          </div>
-        </TabPanel>
-        <TabPanel>
-          <div className="bg-black rounded-md h-[30rem] text-white p-2">
-            /c/Users/mustapha --- 2 ~
-          </div>
-        </TabPanel>
-        <TabPanel>
-          <div className="bg-black rounded-md h-[30rem] text-white p-2">
-            /c/Users/mustapha --- 3 ~
-          </div>
-        </TabPanel>
-      </TabPanels>
-    </div>
+    </TabsContext.Provider>
   );
 }
 
@@ -45,12 +56,18 @@ function TabList({ children }) {
   return <div className="flex gap-1">{children}</div>;
 }
 
-function Tab({ children, active }) {
+function Tab({ children }) {
+  const context = React.useContext(TabsContext);
+  const index = context.numberOfchildren;
+  context.numberOfchildren++;
   return (
     <button
       className={`flex w-48 items-center justify-between shadow-sm border p-3 rounded-lg ${
-        active ? "bg-gray-200" : ""
+        context.clickTabIndex === index ? "bg-gray-200" : ""
       }`}
+      onClick={() => {
+        context.setClickTabIndex(index);
+      }}
     >
       <span>{children}</span>
       <span>
@@ -61,7 +78,8 @@ function Tab({ children, active }) {
 }
 
 function TabPanels({ children }) {
-  return <>{children[0]}</>;
+  const context = React.useContext(TabsContext);
+  return <>{children[context.clickTabIndex]}</>;
 }
 
 function TabPanel({ children }) {
